3c27ebb431b85540c6a19c95602c2176b49b353a
Praveen kusuluri <76720876+PraveenKusuluri08@users.noreply.github.com>
2022-04-20 15:47:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express")
 app.use(express.json())
 app.use("/api", require("./ExpressServices/controller"))
 
-const PORT = 5000
+const PORT = process.env.PORT||5000
 
 app.listen(PORT, () => {
   console.log(`App is listing at ${PORT}`)
